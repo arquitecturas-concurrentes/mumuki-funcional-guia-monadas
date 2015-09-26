854815a65960d3680f2c9aaa73f2787fbbdaaf83
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-09-26 20:40:07 -0300

    Corrigiendo tests

--- 00100_La Estructura/test.hs
+++ 00100_La Estructura/test.hs
@@ -1,12 +1,15 @@
 describe "Validated" $ do
   it "permite construir (Valid 2000000)" $ do
     let patrimonioAsquelCorp = Valid 2000000
+    patrimonioAsquelCorp `shouldBe` patrimonioAsquelCorp
 
-  it "(Valid True) == (Valid True)" $ do
-    (Valid True) `shouldBe` (Valid True)
+  it "show (Valid True) == \"(Valid True)\"" $ do
+    show (Valid True) `shouldBe` "Valid True"
 
   it "permite construir (Doubtful 45 \"contratron mas gente\")" $ do
     let empleadosNoditoInc = Doubtful 45 "contratron mas gente"
+    empleadosNoditoInc `shouldBe` empleadosNoditoInc
 
   it "permite construir (Invalid \"no está mas en la bola\")" $ do
-    let tickerJemaRoja = Invalid "no está mas en la bola"
+    let tickerJemaRoja = Invalid "no está mas en la bola" :: Validated Int
+    tickerJemaRoja `shouldBe` tickerJemaRoja
