adc2e42232b47db137b67be97a2f9ea0a181c58a
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-09-26 20:43:39 -0300

    Corrigiendo tests punto 2

--- 00200_valorMensual/test.hs
+++ 00200_valorMensual/test.hs
@@ -1,10 +1,10 @@
 describe "valorMensual" $ do
-  it "valorMensual (Valid 1200) == (Valid 100)"
+  it "valorMensual (Valid 1200) == (Valid 100)" $ do
     valorMensual (Valid 1200) `shouldBe` (Valid 100)
 
-  it "valorMensual (Invalid \"quebró\") == (Invalid \"quebró\")"
+  it "valorMensual (Invalid \"quebró\") == (Invalid \"quebró\")" $ do
     valorMensual (Invalid "quebró") `shouldBe` (Invalid "quebró")
 
-  it "valorMensual (Doubtful 36 \"incendio en abril\") == (Doubtful 3 \"incendio en abril\")"
+  it "valorMensual (Doubtful 36 \"incendio en abril\") == (Doubtful 3 \"incendio en abril\")" $ do
     valorMensual (Doubtful 36 "incendio en abril") `shouldBe` (Doubtful 3 "incendio en abril")
 
