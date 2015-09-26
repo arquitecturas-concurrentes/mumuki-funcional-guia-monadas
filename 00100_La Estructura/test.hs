e186ec1949df56f0ca9a8ca9e38a6c1b3a9cc33d
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-09-26 20:36:12 -0300

    Agregando $ faltante

--- 00100_La Estructura/test.hs
+++ 00100_La Estructura/test.hs
@@ -1,12 +1,12 @@
 describe "Validated" $ do
-  it "permite construir (Valid 2000000)" do
+  it "permite construir (Valid 2000000)" $ do
     let patrimonioAsquelCorp = Valid 2000000
 
-  it "(Valid True) == (Valid True)" do
+  it "(Valid True) == (Valid True)" $ do
     (Valid True) `shouldBe` (Valid True)
 
-  it "permite construir (Doubtful 45 \"contratron mas gente\")" do
+  it "permite construir (Doubtful 45 \"contratron mas gente\")" $ do
     let empleadosNoditoInc = Doubtful 45 "contratron mas gente"
 
-  it "permite construir (Invalid \"no está mas en la bola\")" do
+  it "permite construir (Invalid \"no está mas en la bola\")" $ do
     let tickerJemaRoja = Invalid "no está mas en la bola"
