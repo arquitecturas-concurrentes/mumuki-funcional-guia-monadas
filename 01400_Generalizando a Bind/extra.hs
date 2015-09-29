7c427592ba878324f6f0496d5673d568150b5f9b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-09-29 16:15:04 -0300

    Agregando funcion faltante

--- 01400_Generalizando a Bind/extra.hs
+++ 01400_Generalizando a Bind/extra.hs
@@ -1,3 +1,7 @@
 data Validated a = Valid a |
                    Invalid String |
                    Doubtful a String deriving (Show, Eq)
+
+validarIguales :: (Eq a, Show a) => a -> a -> Validated a
+validarIguales x y | x == y = Valid x
+                   | otherwise = Invalid (show x++" /= "++show y)
--- 01400_Generalizando a Bind/test.hs
+++ 01400_Generalizando a Bind/test.hs
@@ -3,11 +3,8 @@ describe "bindValidated" $ do
   it "bindValidated (Valid 4) (validarIguales 4) == Valid 4"  $ do
     bindValidated (Valid 4) (validarIguales 4) `shouldBe` Valid 4
 
-  it "bindValidated (Valid True) (\x -> Valid (not x)) == Valid False"  $ do
-    bindValidated (Valid True) (\x -> Valid (not x)) (validarIguales 4) `shouldBe` Valid False
-
   it "bindValidated (Valid 9) (validarIguales 4) == Invalid \"4 /= 9\""  $ do
-    bindValidated (Valid 9) (validarIguales 4) `shouldBe` Invalid "4 /= 9"
+    bindValidated (Valid 9) (validarIguales 4) `shouldBe` (Invalid "4 /= 9")
 
-  it "bindValidated (Invalid \"ups\") (validarIguales 4) == Invalid \"ups\""  $ do
-    bindValidated Invalid "ups" (validarIguales 4) `shouldBe` Invalid "ups"
+  it "bindValidated (Valid True) (\x -> Valid False) == (Valid False)" $ do
+    bindValidated (Valid True) (\x -> Valid False) `shouldBe` (Valid False)
