8f18483591c22ddb750ce70bddd99f15ed6234bc
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-09-29 21:27:27 -0300

    Update extra.hs

--- 01000_Generalizando a ap, cuarta parte/extra.hs
+++ 01000_Generalizando a ap, cuarta parte/extra.hs
@@ -6,3 +6,10 @@ instance Functor (Validated) where
   fmap f (Valid v) = Valid (f v)
   fmap _ (Invalid m) = (Invalid m)
   fmap f (Doubtful v m) = (Doubtful (f v) m)
+
+aplicarValidated (Invalid m) _                 = Invalid m
+aplicarValidated _              (Invalid m)    = Invalid m
+aplicarValidated (Valid f)      (Valid c)      = Valid (f c)
+aplicarValidated (Valid f)      (Doubtful c m) = Doubtful (f c) m
+aplicarValidated (Doubtful f m) (Valid c)      = Doubtful (f c) m
+aplicarValidated (Doubtful f m) (Doubtful c _) = Doubtful (f c) m  
